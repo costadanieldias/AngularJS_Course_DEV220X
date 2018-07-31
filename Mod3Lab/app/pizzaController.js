0212b7796d45807aee6013141e1afc4fb3c0c679
Daniel Dias Costa <Daniel Dias@prodespluz.local>
2018-07-31 09:49:59 -0300

    Finishing the implementation of autocomplete topping search field.

--- Mod3Lab/app/pizzaController.js
+++ Mod3Lab/app/pizzaController.js
@@ -4,11 +4,18 @@ app.controller('pizzaController', [
         $scope.model = {
             title: 'Pizza Builder',
             availableToppings: ['Cheese', 'Pepperoni', 'Bacon', 'Pineapple', 'Sausage', 'Ham', 'Chicken', 'Mushrooms', 'Onion', 'Olives', 'Green Peppers'],
-            toppings: []
+            toppings: [],
+            noResultsMsg: false
         };
         
         $scope.addTopping = function (topping) {
             $scope.model.toppings.push(topping);
+            $scope.model.search = null;
+            $scope.model.successMsg = 'Topping added.';            
+        }
+
+        $scope.hideSuccessMsg = function() {
+            $scope.model.successMsg = null;
         }
     }
 ]);
