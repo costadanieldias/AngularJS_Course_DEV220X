b8576b243bd2779e7aede7d6f26be3ba381efe0f
Daniel Dias Costa <Daniel Dias@prodespluz.local>
2018-08-03 16:43:26 -0300

    Implementing form submission

--- Mod5Lab/app/labController.js
+++ Mod5Lab/app/labController.js
@@ -1,5 +1,17 @@
 app.controller('labController', [
     '$scope',
     function ($scope) {
+        $scope.reset = reset;
+        $scope.submit = submit;
+
+        reset();
+        function reset() {
+            $scope.model = {};
+        }
+
+        function submit(model) {
+            alert('Submitted\n' + JSON.stringify(model));
+        }
+        
     }
 ]);
