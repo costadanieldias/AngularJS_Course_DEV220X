2957c3983e4b0ff996b954768693717d1fbc4f6d
Daniel Dias Costa <Daniel Dias@prodespluz.local>
2018-08-06 10:47:38 -0300

    Implementing form submission to service

--- Mod5Lab/app/labController.js
+++ Mod5Lab/app/labController.js
@@ -1,16 +1,28 @@
 app.controller('labController', [
-    '$scope',
-    function ($scope) {
+    '$scope', 'registration',
+    function ($scope, registration) {
         $scope.reset = reset;
         $scope.submit = submit;
 
         reset();
+
         function reset() {
             $scope.model = {};
         }
 
         function submit(model) {
-            alert('Submitted\n' + JSON.stringify(model));
+            registration.submit(model).$promise
+                .then(function(response) {
+                    console.log(response);
+                    reset();
+                    $scope.token = response.token;
+                    alert('You were registrated successfully! Here is your token: ' + response.token);
+                }, function(response) {
+                    alert('Error');
+                });
+
+
+            //alert('Submitted\n' + JSON.stringify(model));
         }
         
     }
